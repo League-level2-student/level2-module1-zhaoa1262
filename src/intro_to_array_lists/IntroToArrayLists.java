f7e95c06bee5a9f23c888963cda215163eb7fa61
League Student <league@iMac-16.attlocal.net>
2020-01-18 16:34:51 -0800

    //

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -15,18 +15,23 @@ public class IntroToArrayLists {
 		strings.add("flower");
 		
 		//3. Print all the Strings using a standard for-loop
-		for(int i = strings.size(); i > 0; i++) {
+		for(int i = 0; i < strings.size() ; i++) {
 			String sstrings = strings.get(i);
-			//if(i%2 == 1) {
+			if(strings.contains("e")) {
 			System.out.println(sstrings);
-			//}
+			}
 		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
 		
 		//6. Print all the Strings in reverse order.
-		
+		for(int i = strings.size()-1; i >= 0 ; i--) {
+			String sstrings = strings.get(i);
+			//if(i%2 == 1) {
+			System.out.println(sstrings);
+			//}
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
 	}
 }
